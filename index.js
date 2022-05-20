9ddcdbe01363c1387529270fefecea2d08b9c167
Edgar Quansah <quansahedgar@gmail.com>
2022-05-20 17:07:42 -0400

    update @js

---
+++ index.js
@@ -0,0 +1,9 @@
+Using For Loop:
+The basic for loop declaration is shown in the following example. Create a file named ‘for_example.sh’ and add the following script using for loop. Here, for loop will iterate for 10 times and print all values of the variable, counter in single line.
+
+#!/bin/bash
+for (( counter=10; counter>0; counter-- ))
+do
+echo -n "$counter "
+done
+printf "\n"
